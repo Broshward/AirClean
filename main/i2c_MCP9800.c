ae235b6c40029e051262059c6b9f8990f75b8580
Broshward <harmoniesworlds@gmail.com>
2026-03-14 18:45:28 +0300

    Change pins for i2c bus

--- main/i2c_MCP9800.c
+++ main/i2c_MCP9800.c
@@ -2,8 +2,8 @@
 
 #include "i2c_MCP9800.h"
 
-#define I2C_MASTER_SCL_IO           7 //CONFIG_I2C_MASTER_SCL       /*!< GPIO number used for I2C master clock */
-#define I2C_MASTER_SDA_IO           10  //CONFIG_I2C_MASTER_SDA       /*!< GPIO number used for I2C master data  */
+#define I2C_MASTER_SCL_IO           9 //CONFIG_I2C_MASTER_SCL       /*!< GPIO number used for I2C master clock */
+#define I2C_MASTER_SDA_IO           2  //CONFIG_I2C_MASTER_SDA       /*!< GPIO number used for I2C master data  */
 #define I2C_MASTER_NUM              I2C_NUM_0                   /*!< I2C port number for master dev */
 #define I2C_MASTER_FREQ_HZ          100000 //CONFIG_I2C_MASTER_FREQUENCY /*!< I2C master clock frequency */
 #define I2C_MASTER_TX_BUF_DISABLE   0                           /*!< I2C master doesn't need buffer */
